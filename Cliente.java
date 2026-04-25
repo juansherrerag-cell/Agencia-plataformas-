5354eabb5bd0a675dc8123b3de50caf31e80fa87
juansherrerag-cell <juans.herrerag@autonoma.edu.co>
2026-04-25 16:02:18 -0500

    habian cosas que no estaban cerradas como algunas comillas y corchetes,y tambien habian unos codigos que estaban malos,se le agregaron 3 atributos a las clase cliente

--- Cliente.java
+++ Cliente.java
@@ -1,37 +1,53 @@
 public class Cliente {
-    
+
     protected String nombre;
     protected String apellido;
     protected String telefono;
     protected String cedula;
-    
-    
-    public Cliente(String nombre, String apellido, String telefono,String cedula ) {
+    protected String forma_de_pago;
+    protected String lugares_mas_frecuentes;
+    protected String lugar_de_partida;
+
+    public Cliente(String nombre, String apellido, String telefono, String cedula, String forma_de_pago, String lugares_mas_frecuentes,String lugar_de_partida) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
-        this.cedula=cedula;
+        this.cedula = cedula;
+        this.forma_de_pago = forma_de_pago;
+        this.lugares_mas_frecuentes=lugares_mas_frecuentes;
+        this. lugar_de_partida=lugar_de_partida;
     }
 
-    public String getNombre(){return nombre;}
-    public String getApellido(){return apellido;}
-    public String getTelefono(){return telefono;}
-    public String getCedula(){return cedula;}
-     
-     
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getTelefono() { return telefono; }
+    public String getCedula() { return cedula; }
+    public String getFormaDePago() { return forma_de_pago; }
+    public String getLugaresMasFrecuentes(){ return lugares_mas_frecuentes;}
+    public String getLugarDePartida(){ return lugar_de_partida;}
+
     public String getNombreCompleto() {
         return nombre + " " + apellido;
     }
 
     public void mostrarInfo() {
         System.out.println("Nombre: " + getNombreCompleto());
-        System.out.println("Telefono: " + telefono);
+        System.out.println("Teléfono: " + telefono);
+        System.out.println("lugar_de_partida:" + lugar_de_partida);
     }
-    public RegistraCliente(){
 
-        System.out.println("El cliente ha sido registrado exitosamente");    
+    public void registrarCliente() {
+        System.out.println("El cliente ha sido registrado exitosamente.");
     }
-    public void MostrarInformacion() {System.out.println(Informacion del clliente \n};
-     {System.out.println(nombre+""+apellido""+correo)};
 
+    public void mostrarInformacion() {
+        System.out.println("Información del cliente:");
+        System.out.println("Nombre: " + nombre + " " + apellido);
+        System.out.println("Teléfono: " + telefono);
+        System.out.println("Cédula: " + cedula);
+        System.out.println("Forma de pago: " + forma_de_pago);
+        System.out.println("Lugar mas frecuentes: " + lugares_mas_frecuentes);
+        System.out.println("lugar_de_partida:" + lugar_de_partida);
+    }
+}
  
